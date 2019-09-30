b50c70b3196d9de07dabf5cd0301165337fb3266
dachosen1 <anderson.nelson1@gmail.com>
2019-09-30 17:36:24 -0400

    Add todo list for Anderson.

--- Models/mouse_tracking_algorithm.py
+++ Models/mouse_tracking_algorithm.py
@@ -1,17 +1,36 @@
 import math
 import sys
 import time
+from datetime import datetime
 
 from PyQt5.QtGui import QCursor
 from PyQt5.QtWidgets import QApplication
 
 
 class Frame:
+    """
+    The purpose of this class is to replicate the mouse movement done by the Dotin team in oder to discover patterns in
+    user who are falsely filling out survey questions.
+
+    The document contains  a series of function to return coordinates, speed, and direction of mouse movements.
+
+    """
+
+    # TODO: document function
+
     def __init__(self, position, time):
         self.position = position
         self.time = time
 
     def speed(self, frame):
+        """
+        #TODO: document function
+        Function that returns the speed from a coordinate
+
+        :param frame:
+        :return:
+        """
+
         d = distance(*self.position, *frame.position)
         time_delta = abs(frame.time - self.time)
         if time_delta == 0:
@@ -21,10 +40,21 @@ class Frame:
 
 
 def distance(x1, y1, x2, y2):
+    """
+    #TODO: Document function
+
+    :param x1:
+    :param y1:
+    :param x2:
+    :param y2:
+    :return:
+    """
+
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
 def get_current_cursor_position():
+    # TODO: document function:
     pos = QCursor.pos()
     return pos.x(), pos.y()
 
@@ -35,11 +65,16 @@ def get_current_frame():
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     last_frame = get_current_frame()
+    begin = datetime.now()
+
+    Name = input()
+    Intention = input()
 
     while True:
         new_frame = get_current_frame()
-        print(new_frame.position)
+        print(datetime.now(), new_frame.position)
 
-        time.sleep(0.1)
+        time.sleep(1)
+
+# TODO: Write function that saves mouse movements in a csv file with users name
