47e32482fe879d8b53e37798770af11cd406e68a
[dachosen1] <[anderson.nelson1@gmail.com]>
2019-11-08 23:12:41 -0500

    Progress on the hmm model

--- Models/Q1_Mouse Activity/Hidden Markov/hmm_dotin.py
+++ Models/Q1_Mouse Activity/Hidden Markov/hmm_dotin.py
@@ -1,9 +1,10 @@
 from __future__ import print_function, division
-from builtins import range
 
-import numpy as np
-import matplotlib.pyplot as plt
+from builtins import range
 from datetime import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def random_normalized(d1, d2):
@@ -12,34 +13,31 @@ def random_normalized(d1, d2):
 
 
 class HMM:
-    def __init__(self, M):
+    def __init__(self, hidden_states):
         """
-        :param M: number of hidden states
+        :param hidden_states: number of hidden states
         """
-        self.M = M
+        self.hidden_states = hidden_states
 
-    def fit(self, X, max_iter = 30):
+    def fit(self, data, max_iter = 30):
 
         """
         Train the HMM model using the Baum-Welch algorithm a specific instance of the expectation-maximization algorithm
         determine V, the vocabulary size.
 
         This algorithm function assumes that the observation are already in integers ranging from 0 - V-1
-        :param X: a jagged array of observed sequences
+        :param data: a jagged array of observed sequences
         :param max_iter: maximum number of iterations
         """
         t0 = datetime.now()
-        np.random.seed(123)
+        np.random.seed(987)
 
-        V = max(max(x) for x in X) + 1
-        N = len(X)
+        V = max(max(x) for x in data) + 1
+        n_row_data = len(data)
 
-        self.pi = np.ones(self.M) / self.M  # initial state distribution
-        self.A = random_normalized(self.M, self.M)  # state transition matrix
-        self.B = random_normalized(self.M, V)  # output distribution
-
-        print("initial A:", self.A)
-        print("initial B:", self.B)
+        self.states_distribution = np.ones(self.hidden_states) / self.hidden_states
+        self.transition_matrix = random_normalized(self.hidden_states, self.hidden_states)
+        self.output_distribution = random_normalized(self.hidden_states, V)  
 
         costs = []
         for it in range(max_iter):
@@ -47,22 +45,22 @@ class HMM:
                 print("it:", it)
             alphas = []
             betas = []
-            P = np.zeros(N)
-            for n in range(N):
-                x = X[n]
+            P = np.zeros(n_row_data)
+            for n in range(n_row_data):
+                x = data[n]
                 T = len(x)
-                alpha = np.zeros((T, self.M))
-                alpha[0] = self.pi * self.B[:, x[0]]
+                alpha = np.zeros((T, self.hidden_states))
+                alpha[0] = self.states_distribution * self.output_distribution[:, x[0]]
                 for t in range(1, T):
-                    tmp1 = alpha[t - 1].dot(self.A) * self.B[:, x[t]]
+                    tmp1 = alpha[t - 1].dot(self.transition_matrix) * self.output_distribution[:, x[t]]
                     alpha[t] = tmp1
                 P[n] = alpha[-1].sum()
                 alphas.append(alpha)
 
-                beta = np.zeros((T, self.M))
+                beta = np.zeros((T, self.hidden_states))
                 beta[-1] = 1
                 for t in range(T - 2, -1, -1):
-                    beta[t] = self.A.dot(self.B[:, x[t + 1]] * beta[t + 1])
+                    beta[t] = self.transition_matrix.dot(self.output_distribution[:, x[t + 1]] * beta[t + 1])
                 betas.append(beta)
 
             assert (np.all(P > 0))
@@ -70,35 +68,37 @@ class HMM:
             costs.append(cost)
 
             # now re-estimate pi, A, B
-            self.pi = np.sum((alphas[n][0] * betas[n][0]) / P[n] for n in range(N)) / N
+            self.states_distribution = np.sum(
+                (alphas[n][0] * betas[n][0]) / P[n] for n in range(n_row_data)) / n_row_data
 
-            den1 = np.zeros((self.M, 1))
-            den2 = np.zeros((self.M, 1))
+            den1 = np.zeros((self.hidden_states, 1))
+            den2 = np.zeros((self.hidden_states, 1))
             a_num = 0
             b_num = 0
-            for n in range(N):
-                x = X[n]
+            for n in range(n_row_data):
+                x = data[n]
                 T = len(x)
                 den1 += (alphas[n][:-1] * betas[n][:-1]).sum(axis = 0, keepdims = True).T / P[n]
                 den2 += (alphas[n] * betas[n]).sum(axis = 0, keepdims = True).T / P[n]
 
-                a_num_n = np.zeros((self.M, self.M))
-                for i in range(self.M):
-                    for j in range(self.M):
+                a_num_n = np.zeros((self.hidden_states, self.hidden_states))
+                for i in range(self.hidden_states):
+                    for j in range(self.hidden_states):
                         for t in range(T - 1):
-                            a_num_n[i, j] += alphas[n][t, i] * self.A[i, j] * self.B[j, x[t + 1]] * betas[n][t + 1, j]
+                            a_num_n[i, j] += alphas[n][t, i] * self.transition_matrix[i, j] * self.output_distribution[
+                                j, x[t + 1]] * betas[n][t + 1, j]
                 a_num += a_num_n / P[n]
 
-                b_num_n2 = np.zeros((self.M, V))
-                for i in range(self.M):
+                b_num_n2 = np.zeros((self.hidden_states, V))
+                for i in range(self.hidden_states):
                     for t in range(T):
                         b_num_n2[i, x[t]] += alphas[n][t, i] * betas[n][t, i]
                 b_num += b_num_n2 / P[n]
-            self.A = a_num / den1
-            self.B = b_num / den2
-        print("A:", self.A)
-        print("B:", self.B)
-        print("pi:", self.pi)
+            self.transition_matrix = a_num / den1
+            self.output_distribution = b_num / den2
+        # print("A:", self.A)
+        # print("B:", self.B)
+        # print("pi:", self.pi)
 
         print("Fit duration:", (datetime.now() - t0))
 
@@ -112,10 +112,10 @@ class HMM:
         """
 
         T = len(x)
-        alpha = np.zeros((T, self.M))
-        alpha[0] = self.pi * self.B[:, x[0]]
+        alpha = np.zeros((T, self.hidden_states))
+        alpha[0] = self.states_distribution * self.output_distribution[:, x[0]]
         for t in range(1, T):
-            alpha[t] = alpha[t - 1].dot(self.A) * self.B[:, x[t]]
+            alpha[t] = alpha[t - 1].dot(self.transition_matrix) * self.output_distribution[:, x[t]]
         return alpha[-1].sum()
 
     def likelihood_multi(self, X):
@@ -130,13 +130,13 @@ class HMM:
         """
 
         T = len(x)
-        delta = np.zeros((T, self.M))
-        psi = np.zeros((T, self.M))
-        delta[0] = self.pi * self.B[:, x[0]]
+        delta = np.zeros((T, self.hidden_states))
+        psi = np.zeros((T, self.hidden_states))
+        delta[0] = self.states_distribution * self.output_distribution[:, x[0]]
         for t in range(1, T):
-            for j in range(self.M):
-                delta[t, j] = np.max(delta[t - 1] * self.A[:, j]) * self.B[j, x[t]]
-                psi[t, j] = np.argmax(delta[t - 1] * self.A[:, j])
+            for j in range(self.hidden_states):
+                delta[t, j] = np.max(delta[t - 1] * self.transition_matrix[:, j]) * self.output_distribution[j, x[t]]
+                psi[t, j] = np.argmax(delta[t - 1] * self.transition_matrix[:, j])
 
         # backtrack
         states = np.zeros(T, dtype = np.int32)
